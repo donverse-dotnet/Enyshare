a38d8e8fb411271a25864c30bae6f1d1059d458d
KaRU3-dev <himaraya2500m@gmail.com>
2025-06-09 21:59:02 +0900

    スタイルを適用。

--- test/CDN/src/CDNServer/Program.cs
+++ test/CDN/src/CDNServer/Program.cs
@@ -1,26 +1,23 @@
 #pragma warning disable IDE0130
 namespace Pocco.CDN;
 
-public class Server
-{
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+public class Server {
+  public static void Main(string[] args) {
+    var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddOpenApi();
-        builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+    builder.Services.AddControllers();
 
-        var app = builder.Build();
+    var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseRouting();
-        app.MapControllers();
-
-        app.Run();
+    if (app.Environment.IsDevelopment()) {
+      app.MapOpenApi();
     }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    app.MapControllers();
+
+    app.Run();
+  }
 }
