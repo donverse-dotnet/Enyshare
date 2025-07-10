3582a828f394e77b6b5c011f97e186ff9f2ddfc7
KaRU3-dev <himaraya2500m@gmail.com>
2025-07-11 06:51:03 +0900

    gRPC関連サービスの依存関係の注入。

--- src/EventBridge/src/service/Program.cs
+++ src/EventBridge/src/service/Program.cs
@@ -18,10 +18,13 @@ builder.Services.AddSingleton<EventDeployInvoker>();
 builder.Services.AddSingleton<EventStoreTasksDeployer>();
 
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapGrpcService<EventDispatchGrpcService>();
+app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
