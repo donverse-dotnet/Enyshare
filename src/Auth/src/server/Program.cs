b870ede5f0756ab7521fcba3b6b4ca70399e2428
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-07-29 17:33:07 +0900

    サービスの依存関係を注入。

--- src/Auth/src/server/Program.cs
+++ src/Auth/src/server/Program.cs
@@ -1,20 +1,15 @@
 using Pocco.Srv.Auth.Services;
-using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-
-builder.Services.AddSingleton<IMongoClient>(sp =>
-new MongoClient("mongodb://localhost:27017"));
-
-builder.Services.AddSingleton(sp =>
-sp.GetRequiredService<IMongoClient>().GetDatabase("MyAppDb"));
+builder.Services.AddSingleton<JwtTokenHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapGrpcService<V0AuthServiceImpl>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
