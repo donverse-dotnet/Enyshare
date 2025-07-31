69c508faba5d7385aa2d72d4f4e97e38e724bb3c
Masaya1022 <hyn.masaya.h@gmail.com>
2025-07-31 17:26:11 +0900

    プロトコルバッファファイルを追加し、サービスを作成
    -Accounts.csprojにプロトコルバッファファイルを追加
    -program.csにDebugで動かせるようにコードを追加
    -UserAccountsServiceのコレクションの_accountsettingを削除
    -RegisterAccountにアカウントを登録するコードを追加
    -UpdateAccountにアカウントを更新するコードを追加
    -DeleteAccountにアカウントを削除するコードを追加

--- src/Accounts/src/service/Services/UserAccountsService.cs
+++ src/Accounts/src/service/Services/UserAccountsService.cs
@@ -3,110 +3,112 @@ using Grpc.Core;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
-using Pocco.Svc.Accounts.Protos.Account;
-using Pocco.Svc.Accounts.Mappers;
+using Pocco.Libs.Protobufs.Services;
+using Pocco.Libs.Protobufs.Types;
 using Pocco.Svc.Accounts.Models;
 
 
 namespace Pocco.Svc.Accounts.Services;
 
 
-public class UserAccountsService : UserAccounts.UserAccountsBase {
+public class UserAccountsService : V0AccountService.V0AccountServiceBase {
   private readonly IMongoCollection<Account> _accounts;
-  private readonly IMongoCollection<Settings> _accountsettings;
 
   public UserAccountsService(IMongoClient mongoClient) {
     var database = mongoClient.GetDatabase("Entities");
     _accounts = database.GetCollection<Account>("Accounts");
-    _accountsettings = database.GetCollection<Settings>("AccountSettings");
   }
 
-  public override async Task<RegisterAccountReply> RegisterAccount(RegisterAccountRequest request, ServerCallContext context) {
+  public override async Task<V0RegisterReply> Register(V0RegisterRequest request, ServerCallContext context) {
     var model = new Account {
       Email = request.Email,
-      Username = string.Empty,
-      AvatarUrl = string.Empty,
-      StatusMessage = string.Empty,
-      CreateAt = DateTime.UtcNow,
-      IsEmailVerified = false
+      IsEmailVerified = false,
+      PasswordHash = request.Password,
+      Username = request.Email.Split('@')[0],
+      Role = "User",
+      IsActive = true,
+
+      CreatedAt = DateTime.UtcNow,
+      UpdatedAt = DateTime.UtcNow
     };
 
     await _accounts.InsertOneAsync(model);
-    return new RegisterAccountReply {
-      Succes = true,
-      Message = "Account Registed successfully."
+    return new V0RegisterReply {
+      Success = true,
+      Message = "Account Register successfully."
     };
   }
 
-  public override async Task<UpdateAccountReply> UpdateAccount(UpdateAccountRequest request, ServerCallContext context) {
+  public override async Task<V0UpdateReply> Update(V0UpdateRequest request, ServerCallContext context) {
 
-    var filter = Builders<Account>.Filter.Eq(a => a.id, ObjectId.Parse(request.Id));
-    var updateBuilder = Builders<Account>.Update;
+    var filter = Builders<Account>.Filter.Eq(a => a.Id, ObjectId.Parse(request.Id));
+    var updateDataBuilder = Builders<Account>.Update;
     var updates = new List<UpdateDefinition<Account>>();
 
-    updates.Add(updateBuilder.Set(a => a.Username, request.Name)
-    .Set(a => a.Email, request.Email)
-    .Set(a => a.AvatarUrl, request.IconData)
-    .Set(a => a.StatusMessage, request.StatusMessage)
-    .Set(a => a.Role, request.Role)
-    .Set(a => a.PasswordHash, request.Password));
-    if (request.UpdateAt) {
-      updates.Add(updateBuilder.Set(a => a.UpdateAt, DateTime.UtcNow));
+    // 1.パスワード以外を更新
+    updates.Add(updateDataBuilder
+    .Set(acc => acc.Username, request.Name)
+    .Set(acc => acc.Email, request.Email)
+    .Set(acc => acc.AvatarUrl, request.AvatarUrl)
+    .Set(acc => acc.Status, new V0AccountStatusMessageWrapper(request.Status.Status, request.Status.Message))
+    .Set(acc => acc.Notifications, new V0AccountNotificationSettingWrapper(
+      request.Notifications.Email,
+      request.Notifications.Push,
+      request.Notifications.ShowBadge
+    ))
+    );
+    // 2.パスワードを更新
+    if (!string.IsNullOrEmpty(request.Password) || !string.IsNullOrEmpty(request.OnetimeCode)) {
+      //  2.1　ワンタイムコードを比較
+      var account = _accounts
+      .Find(acc => acc.Id == ObjectId.Parse(request.Id) && acc.Onetimecode == request.OnetimeCode)
+      .FirstOrDefault();
+
+      if (account is null) {
+        return new V0UpdateReply {
+          Success = false,
+          Message = "Invalid one-time code or accound not found."
+        };
+      }
+      //  2.2　パスワードを更新
+      updates.Add(updateDataBuilder.Set(acc => acc.PasswordHash, request.Password));
     }
-
-    if (request.PasswordUpdateAt) {
-      updates.Add(updateBuilder.Set(a => a.PasswordUpdateAt, DateTime.UtcNow));
-    }
-
-    if (request.EmailUpdateAt) {
-      updates.Add(updateBuilder.Set(a => a.EmailUpdateAt, DateTime.UtcNow));
-    }
-
-    if (request.LastLoginAt) {
-      updates.Add(updateBuilder.Set(a => a.LastLoginAt, DateTime.UtcNow));
-    }
-    var update = updateBuilder.Combine(updates);
-    var result = await _accounts.UpdateOneAsync(filter, update);
-
-    var uiSetting = AccountSettingMapper.ToModel(request.AccountUisettings, request.Id);
-    uiSetting.UserId = request.Id;
-
-    var uiFilter = Builders<Settings>.Filter.Eq(u => u.UserId, request.Id);
-    await _accountsettings.ReplaceOneAsync(uiFilter, uiSetting, new ReplaceOptions { IsUpsert = true });
-
+    // 3.データベースを更新
+    var result = await _accounts.UpdateOneAsync(filter, updateDataBuilder.Combine(updates));
+    // 4.リプライ
     if (result.ModifiedCount > 0) {
-      return new UpdateAccountReply {
-        Succes = true,
+      return new V0UpdateReply {
+        Success = true,
         Message = "Account updated successfully."
       };
     } else {
-      return new UpdateAccountReply {
-        Succes = false,
+      return new V0UpdateReply {
+        Success = false,
         Message = "No account was updated. Check if the ID is correct."
       };
     }
-
-
   }
 
-  public override async Task<DeleteAccountReply> DeleteAccount(DeleteAccountRequest request, ServerCallContext context) {
-    if (!ObjectId.TryParse(request.AccountId, out var objectId)) {
-      return new DeleteAccountReply {
-        Succes = false,
-        Message = "Invalid account ID format."
-      };
-    }
+  public override async Task<V0DeleteReply> Delete(V0DeleteRequest request, ServerCallContext context) {
 
-    var result = await _accounts.DeleteOneAsync(a => a.id == objectId);
+    var filter = Builders<Account>.Filter.Eq(a => a.Id, ObjectId.Parse(request.Id));
+    var updateDataBuilder = Builders<Account>.Update;
+    var updates = new List<UpdateDefinition<Account>>() {
+      updateDataBuilder.Set(acc => acc.IsActive, false),
+      updateDataBuilder.Set(acc => acc.UpdatedAt, DateTime.UtcNow),
+      updateDataBuilder.Set(acc => acc.DeletionRequestedAt, DateTime.UtcNow)
+    };
 
-    if (result.DeletedCount > 0) {
-      return new DeleteAccountReply {
-        Succes = true,
+    var result = await _accounts.UpdateOneAsync(filter, updateDataBuilder.Combine(updates));
+
+    if (result.ModifiedCount > 0) {
+      return new V0DeleteReply {
+        Success = true,
         Message = "Account deleted successfully."
       };
     } else {
-      return new DeleteAccountReply {
-        Succes = false,
+      return new V0DeleteReply {
+        Success = false,
         Message = "Account not found or already deleted."
       };
     }
