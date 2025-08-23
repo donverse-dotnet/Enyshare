9b2c4bf0b7c739cb09271936b7ce90e59b50bc18
Masaya1022 <hyn.masaya.h@gmail.com>
2025-08-23 19:38:59 +0900

    プロトコルバッファファイルを呼ぶC#コードを追加
    OrganizationRoleService.csを追加

---
+++ src/Organizations/src/servers/RoleService/Services/OrganizationRoleService.cs
@@ -0,0 +1,11 @@
+using Grpc.Core;
+
+using Pocco.Libs.Protobufs.Services;
+using Pocco.Libs.Protobufs.Types;
+
+namespace RoleService.Service;
+
+public class OrganizationRoleService : V0RoleService.V0RoleServiceBase
+{
+    
+}
