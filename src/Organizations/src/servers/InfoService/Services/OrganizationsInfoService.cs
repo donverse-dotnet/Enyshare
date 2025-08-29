fc417ad071f08c3ad75eeb4447e2635cc3ca4e59
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-08-30 02:14:20 +0900

    InfoServiceのprotoファイルを作成

---
+++ src/Organizations/src/servers/InfoService/Services/OrganizationsInfoService.cs
@@ -0,0 +1,10 @@
+using Grpc.Core;
+using Pocco.Libs.Protobufs.Service;
+using Pocco.Libs.Protobufs.Types;
+
+namespace InfoService.Services;
+
+public class OrganizationsInfoService : VOInfoService.VOInfoServiceBase
+{
+
+}
