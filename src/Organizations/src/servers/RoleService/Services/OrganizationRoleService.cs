79b1291ce11cc4b7d2dbc0a788346587a06fa130
Masaya1022 <hyn.masaya.h@gmail.com>
2025-08-31 03:11:13 +0900

    OrganizzationRoleServiceにCreateとUpdateを作成
    -CreateとUpdateを作成中

--- src/Organizations/src/servers/RoleService/Services/OrganizationRoleService.cs
+++ src/Organizations/src/servers/RoleService/Services/OrganizationRoleService.cs
@@ -11,7 +11,7 @@ using Pocco.Svc.Roles.Models;
 
 namespace RoleService.Services;
 
-public class OrganizationRoleService : V0OrganizationRoleService.V0OrganizationRoleServiceBase {
+public class OrganizationRoleService : V0RoleService.V0RoleServiceBase {
     private readonly IMongoCollection<Role> _roles;
     public OrganizationRoleService(IMongoClient mongoClient) {
         var database = mongoClient.GetDatabase("Entities");
@@ -20,37 +20,37 @@ public class OrganizationRoleService : V0OrganizationRoleService.V0OrganizationR
 
     public override async Task<V0CreateReply> Create(V0CreateRequest request, ServerCallContext callContext) {
 #pragma warning disable CS8601 // Null 参照代入の可能性があります。
-    var model = new Role {
+        var model = new Role {
             Name = request.Name,
             Description = Console.ReadLine()?.Trim(),
             Permissions = Console.ReadLine()?.Trim()?
-            .Split(',')                             //カンマで分割
-            .Select(p => p.Trim())                  //各要素の前後の空白を削除
-            .Where(p => !string.IsNullOrEmpty(p))   //空要素を除外
-            .ToList() ?? new List<string>(),         //null対策
+                .Split(',')                             //カンマで分割
+                .Select(p => p.Trim())                  //各要素の前後の空白を削除
+                .Where(p => !string.IsNullOrEmpty(p))   //空要素を除外
+                .ToList() ?? new List<string>(),         //null対策
             CreatedAt = DateTime.UtcNow,
             UpdatedAt = DateTime.UtcNow
         };
 
-    await _roles.InsertOneAsync(model);
+        await _roles.InsertOneAsync(model);
         return new V0CreateReply {
-            Success = true,
+
         };
     }
 
-    /*public override async Task<Empty> Update(V0RoleModel rolemodel, ServerCallContext context) {
-        var filter = Builders<Role>.Filter.Eq(a => a.Id, ObjectId.Parse(rolemodel.Id));
+    /*public override async Task<Empty> Update(V0UpdateRequest request, ServerCallContext context) {
+        var filter = Builders<Role>.Filter.Eq(a => a.Id, ObjectId.Parse(request.Id));
         var updateDataBuilder = Builders<Role>.Update;
         var updates = new List<UpdateDefinition<Role>>();
 
         updates.Add(updateDataBuilder
-        .Set(ro => ro.Name, rolemodel.Name)
-        .Set(ro => ro.Description, rolemodel.Descriptions)
-        .Set(ro => ro.Permissions, rolemodel.Permissions.Split(',').Select(s => s.Trim()).ToList())
+        .Set(ro => ro.Name, request.Name)
+        .Set(ro => ro.Description, request.Descriptions)
+        .Set(ro => ro.Permissions, request.Permissions.Split(',').Select(s => s.Trim()).ToList())
         );
 
         await _roles.UpdateOneAsync(filter, updateDataBuilder.Combine(updates));
-        return 
+        return new Empty();
     }*/
 }
 
