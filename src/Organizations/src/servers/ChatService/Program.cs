6fc600ce1e7cd775f943019df589ad623576897c
Masaya1022 <hyn.masaya.h@gmail.com>
2025-09-06 19:35:10 +0900

    DEBUGを含め、OrganizationChatServiceを動かすコードを追加
    -#if #else #endifを追加
    -MongoClientを追加
    -GrpcReflectionを追加
    -MapGrpcService<OrganizationChatService>を追加
    -"TEST_DATABASE_URI"と見つからなかった場合を追加

--- src/Organizations/src/servers/ChatService/Program.cs
+++ src/Organizations/src/servers/ChatService/Program.cs
@@ -1,6 +1,37 @@
-using ChatService.Services;
+using Pocco.Svc.ChatService.Services;
+using MongoDB.Driver;
+
+#if DEBUG
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IMongoClient>(sp => {
+  var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_URI") ?? throw new ArgumentException("TEST_DATABASE_URI is not found");
+  return new MongoClient(connectionString);
+
+});
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<OrganizationChatService>();
+app.MapGrpcReflectionService();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
+
+#else
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IMongoClient>(sp => {
+  var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_URI") ?? throw new ArgumentException("TEST_DATABASE_URI is not found");
+  return new MongoClient(connectionString);
+
+});
 
 // Add services to the container.
 builder.Services.AddGrpc();
@@ -8,7 +39,10 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapGrpcService<OrganizationChatService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+#endif
