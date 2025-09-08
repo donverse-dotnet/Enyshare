b19556b5099eeea4db0966001e29d40d790591e2
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-09-09 04:01:40 +0900

    利用しないため、GreeterServiceを削除。

--- src/CoreAPI/src/service/Program.cs
+++ src/CoreAPI/src/service/Program.cs
@@ -2,7 +2,7 @@ using Grpc.Net.Client;
 using Microsoft.AspNetCore.Authentication;
 using Pocco.Libs.Protobufs.Services;
 using Pocco.Svc.CoreAPI.Handlers;
-using Pocco.Svc.CoreAPI.Services;
+// using Pocco.Svc.CoreAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,7 +26,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapGrpcService<GreeterService>();
+// Add GrpcServices to here.
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
