617da5bc8e222c39da6f89fb4f5dc28c7e672d2c
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-09-27 17:27:16 +0900

    不要なファイルを削除
    サービスに必要のないファイルだったため削除。

--- src/Organizations/src/servers/InfoService/Services/ChatEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace InfoService.Services;
-
-public class ChatEntity
-{
-  public string Id { get; set; }
-  public string OrganizationId { get; set; }
-  public string Name { get; set; }
-  public DateTime CreatedAt { get; set; }
-}
--- src/Organizations/src/servers/InfoService/Services/RoleEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace InfoService.Services;
-
-public class RoleEntity
-{
-  public string Id { get; set; }
-  public string OrganizationId { get; set; }
-  public string Name { get; set; }
-  public string[] Permissions { get; set; }
-}
