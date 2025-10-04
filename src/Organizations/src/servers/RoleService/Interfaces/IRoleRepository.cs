f7396f647c3fdfb60781e0fc9ccebcbfd67af44d
Masaya1022 <hyn.masaya.h@gmail.com>
2025-10-04 18:12:36 +0900

    TryUpdateAsyncの戻り値をboolに変え、適したメソッド名に変更。
    -Task<bool> TryUpdateAsyncに変更。

--- src/Organizations/src/servers/RoleService/Interfaces/IRoleRepository.cs
+++ src/Organizations/src/servers/RoleService/Interfaces/IRoleRepository.cs
@@ -3,6 +3,6 @@ using Pocco.Svc.Roles.Models;
 public interface IRoleRepository {
   Task<Role> GetByIdAsync(string org_Id, string id);
   Task<Role> CreateAsync(string org_Id, Role role);
-  Task<Role> UpdateAsync(string org_Id, string id, Role updaterole);
+  Task<bool> TryUpdateAsync(string org_Id, string id, Role updaterole);
   Task<bool> DeleteAsync(string org_Id, string id);
 }
