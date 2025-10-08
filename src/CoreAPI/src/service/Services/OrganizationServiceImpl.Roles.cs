54ba68a797393f07df30fb3d47f89c6f17029433
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-10-08 10:57:28 +0900

    組織のロールを操作できるgRPCメソッドを実装。

--- src/CoreAPI/src/service/Services/OrganizationServiceImpl.Roles.cs
+++ src/CoreAPI/src/service/Services/OrganizationServiceImpl.Roles.cs
@@ -5,18 +5,44 @@ using Pocco.Libs.Protobufs.Services;
 namespace Pocco.Svc.CoreAPI.Services;
 
 public partial class OrganizationsServiceImpl : V0OrganizationsService.V0OrganizationsServiceBase {
-  public override Task<GetOrganizationResponse> Get(GetOrganizationRequest request, ServerCallContext context) {
-    // TODO: Get organization data from service.
+  // org.RolesService.RolesServiceClient <- コンストラクタで受け取れるようにする
 
-    // Send default response
-    return base.Get(request, context);
+  // ListRoles
+  public override async Task<V0ListRolesResponse> ListRoles(V0ListXRequest request, ServerCallContext context) {
+    // ここにorg.RolesService.RolesServiceClientを使ったロジックを実装
+    // 例として、空のレスポンスを返す
+    var response = new V0ListRolesResponse();
+    // response.Roles.Add(...); // 必要に応じてロールを追加
+    return await Task.FromResult(response);
   }
 
-  public override Task<ListOrganizationsResponse> List(Empty request, ServerCallContext context) {
-    return base.List(request, context);
+  // GetRole
+  public override async Task<Role> GetRole(V0BaseRequest request, ServerCallContext context) {
+    // ここにorg.RolesService.RolesServiceClientを使ったロジックを実装
+    // 例として、ダミーのロール情報を返す
+    var role = new Role();
+
+    return await Task.FromResult(role);
   }
 
-  // roles
-  // members
-  // chats
+  // CreateRole
+  public override Task<Empty> CreateRole(V0CreateXRequest request, ServerCallContext context) {
+    // ここにorg.RolesService.RolesServiceClientを使ったロジックを実装
+
+    return Task.FromResult(new Empty());
+  }
+
+  // UpdateRole
+  public override Task<Empty> UpdateRole(Role request, ServerCallContext context) {
+    // ここにorg.RolesService.RolesServiceClientを使ったロジックを実装
+
+    return Task.FromResult(new Empty());
+  }
+
+  // DeleteRole
+  public override Task<Empty> DeleteRole(V0BaseRequest request, ServerCallContext context) {
+    // ここにorg.RolesService.RolesServiceClientを使ったロジックを実装
+
+    return Task.FromResult(new Empty());
+  }
 }
