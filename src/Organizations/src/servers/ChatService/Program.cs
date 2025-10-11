99cd1cd692bbe71cdf55ef0fdd5a70e97f748468
Masaya1022 <hyn.masaya.h@gmail.com>
2025-10-11 19:00:19 +0900

    リポジトリをサービスに注入するコードを追加。

--- src/Organizations/src/servers/ChatService/Program.cs
+++ src/Organizations/src/servers/ChatService/Program.cs
@@ -1,5 +1,6 @@
 using Pocco.Svc.ChatService.Services;
 using MongoDB.Driver;
+using Pocco.Svc.Chats.Ripositories;
 
 #if DEBUG
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IMongoClient>(sp => {
   var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_URI") ?? throw new ArgumentException("TEST_DATABASE_URI is not found");
   return new MongoClient(connectionString);
+});
 
+builder.Services.AddSingleton<IChatRepository>(sp => {
+  var mongoClient = sp.GetRequiredService<IMongoClient>();
+  return new ChatRepository(mongoClient);
 });
 
 // Add services to the container.
