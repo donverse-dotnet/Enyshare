e3ad3815ebae3752e90274a67e267841934e454b
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-10-15 02:23:59 +0900

    Serilogを使って、ログの出力と保存ができるように実装。

--- src/CoreAPI/src/service/Program.cs
+++ src/CoreAPI/src/service/Program.cs
@@ -5,8 +5,19 @@ using Pocco.Libs.Protobufs.Services;
 using Pocco.Svc.CoreAPI.Auth;
 using Pocco.Svc.CoreAPI.Services;
 using Pocco.Svc.CoreAPI.Services.Grpc;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Use Serilog for logging
+builder.Host.UseSerilog((ctx, cfg) => {
+  cfg
+    .Enrich.WithThreadId()
+    // Set log style -> [yyyy-MM-dd HH:mm:ss.fff] [Level] [SourceContext][[ThreadId]] Message NewLine Exception
+    .WriteTo.Console(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}] {Level:u4}: {SourceContext}[{ThreadId}] {Message:lj}{NewLine}{Exception}")
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}] {Level:u4}: {SourceContext}[{ThreadId}] {Message:lj}{NewLine}{Exception}")
+    .Enrich.FromLogContext();
+});
 
 // Add services to the container.
 // Hosted services
