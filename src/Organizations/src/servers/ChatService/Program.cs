c740abcf4708540647e6e951ac65672a55c7cc96
Masaya1022 <hyn.masaya.h@gmail.com>
2025-10-18 17:52:28 +0900

    Environment.IsDevelopment()関数を利用してリフレクションを有効化し、不要な#if DEBUG ... #else ... #endif を削除。

--- src/Organizations/src/servers/ChatService/Program.cs
+++ src/Organizations/src/servers/ChatService/Program.cs
@@ -2,7 +2,6 @@ using Pocco.Svc.ChatService.Services;
 using MongoDB.Driver;
 using Pocco.Svc.Chats.Ripositories;
 
-#if DEBUG
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IMongoClient>(sp => {
@@ -17,37 +16,19 @@ builder.Services.AddSingleton<IChatRepository>(sp => {
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddGrpcReflection();
+
+if (builder.Environment.IsDevelopment()) {
+  builder.Services.AddGrpcReflection();
+}
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<OrganizationChatService>();
-app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-app.Run();
-
-#else
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton<IMongoClient>(sp => {
-  var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_URI") ?? throw new ArgumentException("TEST_DATABASE_URI is not found");
-  return new MongoClient(connectionString);
-
-});
-
-// Add services to the container.
-builder.Services.AddGrpc();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<OrganizationChatService>();
-
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+if (app.Environment.IsDevelopment()) {
+  app.MapGrpcReflectionService();
+}
 
 app.Run();
-
-#endif
