45712ca46015e122bb8c0793a76b78590f25b23e
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-10-21 15:55:46 +0900

    ApiServiceの実装部分を作成。

--- src/CoreAPI/src/service/Services/Grpc/ApiServiceImpl.cs
+++ src/CoreAPI/src/service/Services/Grpc/ApiServiceImpl.cs
@@ -4,7 +4,11 @@ using Pocco.Libs.Protobufs.Services;
 namespace Pocco.Svc.CoreAPI.Services.Grpc;
 
 public partial class ApiServiceImpl : V0ApiService.V0ApiServiceBase {
-  public ApiServiceImpl([FromServices] ILogger<ApiServiceImpl> logger) {
+  public ApiServiceImpl(
+    [FromServices] V0AccountService.V0AccountServiceClient asc,
+    [FromServices] ILogger<ApiServiceImpl> logger
+  ) {
+    _accountServiceClient = asc;
     _logger = logger;
 
     _logger.LogInformation("ApiServiceImpl initialized.");
