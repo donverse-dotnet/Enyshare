638ec1a8795adb09052b1ca1d58a4d577c87cf8c
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-10-23 16:25:51 +0900

    処理の共通化と命名規則の修正。

--- src/Organizations/src/servers/MemberService/Repositories/IMemberRepository.cs
+++ src/Organizations/src/servers/MemberService/Repositories/IMemberRepository.cs
@@ -3,6 +3,8 @@ using MemberService.Services;
 
 namespace MemberService.Repositories {
     public interface IMemberRepository {
+        Task<MemberEntity> CreateAsync(string org_id, MemberEntity member);
+        Task<MemberEntity> GetByIdAsync(string org_id, string id);
         Task<MemberEntity> FindByIdAsync(string id);
         Task InsertAsync(MemberEntity entity);
         Task UpdateAsync(MemberEntity entity);
--- src/Organizations/src/servers/MemberService/Repositories/MemberRepository.cs
+++ src/Organizations/src/servers/MemberService/Repositories/MemberRepository.cs
@@ -1,20 +1,45 @@
 using MongoDB.Driver;
 using MongoDB.Bson;
 using MemberService.Services;
+using Grpc.Core;
 
 namespace MemberService.Repositories {
     public class MemberRepository : IMemberRepository {
-        public readonly IMongoCollection<MemberEntity> _collection;
-        public MemberRepository(IMongoDatabase database) {
-            _collection = database.GetCollection<MemberEntity>("Members");
+        private readonly IMongoClient _client;
+        public MemberRepository(IMongoClient client) {
+            _client = client;
         }
 
-        public async Task<MemberEntity> FindByIdAsync(string id) {
-            return await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        private FilterDefinition<MemberEntity> CreateFilter(string Id) {
+            if (!ObjectId.TryParse(Id, out _)) {
+                throw new ArgumentException("Invalid id or chatId format");
+            }
+
+            return Builders<MemberEntity>.Filter.And(
+            Builders<MemberEntity>.Filter.Eq(c => c.Id, Id)
+            );
         }
 
-        public async Task InsertAsync(MemberEntity entity) {
-            await _collection.InsertOneAsync(entity);
+        private IMongoCollection<MemberEntity> GetMongoCollection(string org_id) {
+            if (!ObjectId.TryParse(org_id, out _)) {
+                throw new ArgumentException("Invalid id or Id format");
+            }
+
+            var db = _client.GetDatabase(org_id);
+            return db.GetCollection<MemberEntity>("Members");
+        }
+
+        public async Task<MemberEntity> CreateAsync(string org_id, MemberEntity member) {
+            var members = GetMongoCollection(org_id);
+            await members.InsertOneAsync(member);
+
+            var latestMember = await GetByIdAsync(org_id, member.Id);
+
+            if (latestMember is null) {
+                throw new RpcException(new Status(StatusCode.Internal, $"Member creation failed for {org_id}"));
+            }
+
+            return latestMember;
         }
 
         public async Task UpdateAsync(MemberEntity entity) {
