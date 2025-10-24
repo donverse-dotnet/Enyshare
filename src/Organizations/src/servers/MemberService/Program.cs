3400cbcfbf5f7ad91af665de470e630fd678ef4d
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-10-24 23:55:02 +0900

    Environment.IsDevelopment()関数を利用してリフレクションを有効化。

--- src/Organizations/src/servers/MemberService/Program.cs
+++ src/Organizations/src/servers/MemberService/Program.cs
@@ -14,6 +14,10 @@ builder.Services.AddSingleton(sp => {
 
 builder.Services.AddGrpc();
 
+if (builder.Environment.IsDevelopment()) {
+  builder.Services.AddGrpcReflection();
+}
+
 builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
@@ -22,5 +26,9 @@ var app = builder.Build();
 app.MapGrpcService<OrganizationsMemberServiceImpl>();
 app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+if (app.Environment.IsDevelopment()) {
+  app.MapGrpcReflectionService();
+}
 
 app.Run();
