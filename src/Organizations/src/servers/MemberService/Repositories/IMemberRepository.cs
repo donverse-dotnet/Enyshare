fe554e994156775427ca7d29c16dbfa4483774c8
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-10-24 23:00:29 +0900

    TryUpdateAsyncとDeleteAsyncの追加。不要になったものの削除。

--- src/Organizations/src/servers/MemberService/Repositories/IMemberRepository.cs
+++ src/Organizations/src/servers/MemberService/Repositories/IMemberRepository.cs
@@ -5,9 +5,7 @@ namespace MemberService.Repositories {
     public interface IMemberRepository {
         Task<MemberEntity> CreateAsync(string org_id, MemberEntity member);
         Task<MemberEntity> GetByIdAsync(string org_id, string id);
-        Task<MemberEntity> FindByIdAsync(string id);
-        Task InsertAsync(MemberEntity entity);
-        Task UpdateAsync(MemberEntity entity);
-        Task DeleteAsync(string id);
+        Task<bool> TryUpdateAsync(string org_id, string id, MemberEntity updatemember);
+        Task<bool> DeleteAsync(string org_id, string id);
     }
 }
