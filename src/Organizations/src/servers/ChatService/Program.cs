bbc04d96a66180db0de0ac5fed65a9282292c32d
Masaya1022 <hyn.masaya.h@gmail.com>
2025-10-25 19:22:21 +0900

    EventBridgeにイベントを登録する機能を追加。

--- src/Organizations/src/servers/ChatService/Program.cs
+++ src/Organizations/src/servers/ChatService/Program.cs
@@ -1,12 +1,20 @@
 using Pocco.Svc.ChatService.Services;
 using MongoDB.Driver;
 using Pocco.Svc.Chats.Ripositories;
+using Pocco.Svc.EventBridge.Protobufs.Services;
+using Grpc.Net.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IMongoClient>(sp => {
   var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_URI") ?? throw new ArgumentException("TEST_DATABASE_URI is not found");
   return new MongoClient(connectionString);
+});
+
+builder.Services.AddSingleton<V0EventReceiver.V0EventReceiverClient>(sp => {
+  var eventBridgeUrl = Environment.GetEnvironmentVariable("EVENTBRIDGE_URL") ?? throw new ArgumentException("EVENTBRIDGE_URL is not found");
+  var channel = GrpcChannel.ForAddress(eventBridgeUrl);
+  return new V0EventReceiver.V0EventReceiverClient(channel);
 });
 
 builder.Services.AddSingleton<IChatRepository>(sp => {
--- src/Organizations/src/servers/ChatService/Services/OrganizationChatService.cs
+++ src/Organizations/src/servers/ChatService/Services/OrganizationChatService.cs
@@ -9,6 +9,9 @@ using MongoDB.Bson;
 using Pocco.Libs.Protobufs.Services;
 using Pocco.Libs.Protobufs.Types;
 using Pocco.Svc.Chats.Models;
+using Pocco.Svc.EventBridge.Protobufs.Enums;
+using Pocco.Svc.EventBridge.Protobufs.Services;
+using Pocco.Svc.EventBridge.Protobufs.Types;
 
 using SharpCompress.Common;
 
@@ -17,10 +20,12 @@ namespace Pocco.Svc.ChatService.Services;
 public class OrganizationChatService : V0OrganizationChatService.V0OrganizationChatServiceBase {
   private readonly IChatRepository _repository;
   private readonly ILogger<OrganizationChatService> _logger;
+  private readonly V0EventReceiver.V0EventReceiverClient _eventBridge;
 
-  public OrganizationChatService([FromServices] IChatRepository repository, [FromServices] ILogger<OrganizationChatService> logger) {
+  public OrganizationChatService([FromServices] IChatRepository repository, [FromServices] ILogger<OrganizationChatService> logger, [FromServices] V0EventReceiver.V0EventReceiverClient eventBridge) {
     _repository = repository;
     _logger = logger;
+    _eventBridge = eventBridge;
 
     _logger.LogInformation("OrganizationChatService is initialized!");
   }
@@ -39,8 +44,31 @@ public class OrganizationChatService : V0OrganizationChatService.V0OrganizationC
     };
     Chat createdChat = await _repository.CreateAsync(request.OrgId, chat);
     _logger.LogInformation("{ChatId} is successfully created on {OrgId}", createdChat.Id, request.OrgId);
+
+    //イベントを伝搬させるのをEventBridgeに依頼
+
+    var newEventData = new V0NewEventRequest {
+      Topic = V0EventTopics.EventTopicOrganization,
+      EventType = "OnChatCreated",
+      ApiVersion = "0",
+      InvokedAt = Timestamp.FromDateTime(DateTime.UtcNow),
+      InvokedBy = request.InvokedBy
+    };
+
+    newEventData.Payload.Fields.Add("organization_id", new Value { StringValue = $"{request.OrgId}" });
+    newEventData.Payload.Fields.Add("chat_id", new Value { StringValue = $"{createdChat.Id}" });
+    newEventData.Payload.Fields.Add("name", new Value { StringValue = $"{createdChat.Name}" });
+    newEventData.Payload.Fields.Add("description", new Value { StringValue = $"{createdChat.Description}" });
+    newEventData.Payload.Fields.Add("created_by", new Value { StringValue = $"{createdChat.CreatedBy}" });
+    newEventData.Payload.Fields.Add("created_at", new Value { StringValue = $"{createdChat.CreatedAt}" });
+    newEventData.Payload.Fields.Add("is_private", new Value { StringValue = $"{createdChat.IsPrivate}" });
+
+    var createdEventData = _eventBridge.NewEvent(
+      newEventData
+    );
+
     return new V0ChatChangesReply {
-      EventId = "fake id" //TODO: eventbridgeからのidに置き換える
+      EventId = createdEventData.EventId
     };
   }
 
@@ -59,8 +87,28 @@ public class OrganizationChatService : V0OrganizationChatService.V0OrganizationC
       throw new RpcException(new Status(StatusCode.NotFound, "Chat not found"));
     }
 
+    var newEventData = new V0NewEventRequest {
+      Topic = V0EventTopics.EventTopicOrganization,
+      EventType = "OnChatUpdated",
+      ApiVersion = "0",
+      InvokedAt = Timestamp.FromDateTime(DateTime.UtcNow),
+      InvokedBy = request.InvokedBy
+    };
+
+    newEventData.Payload.Fields.Add("organization_id", new Value { StringValue = $"{request.Chatsmodel.OrgId}" });
+    newEventData.Payload.Fields.Add("chat_id", new Value { StringValue = $"{updateChat.Id}" });
+    newEventData.Payload.Fields.Add("name", new Value { StringValue = $"{updateChat.Name}" });
+    newEventData.Payload.Fields.Add("description", new Value { StringValue = $"{updateChat.Description}" });
+    newEventData.Payload.Fields.Add("created_by", new Value { StringValue = $"{updateChat.CreatedBy}" });
+    newEventData.Payload.Fields.Add("created_at", new Value { StringValue = $"{updateChat.CreatedAt}" });
+    newEventData.Payload.Fields.Add("is_private", new Value { StringValue = $"{updateChat.IsPrivate}" });
+
+    var updatedEventData = _eventBridge.NewEvent(
+      newEventData
+    );
+
     return new V0ChatChangesReply {
-      EventId = "fake id" //TODO: eventbridgeからのidに置き換える
+      EventId = updatedEventData.EventId
     };
   }
 
@@ -79,8 +127,24 @@ public class OrganizationChatService : V0OrganizationChatService.V0OrganizationC
     if (!success) {
       throw new RpcException(new Status(StatusCode.NotFound, "Chat not found or no fields to delete"));
     }
+
+    var newEventData = new V0NewEventRequest {
+      Topic = V0EventTopics.EventTopicOrganization,
+      EventType = "OnChatDeleted",
+      ApiVersion = "0",
+      InvokedAt = Timestamp.FromDateTime(DateTime.UtcNow),
+      InvokedBy = request.InvokedBy
+    };
+
+    newEventData.Payload.Fields.Add("organization_id", new Value { StringValue = $"{request.OrgId}" });
+    newEventData.Payload.Fields.Add("chat_id", new Value { StringValue = $"{request.Id}" });
+
+    var deletedEventData = _eventBridge.NewEvent(
+     newEventData
+   );
+
     return new V0ChatChangesReply {
-      EventId = "fake id" //TODO: eventbridgeからのidに置き換える
+      EventId = deletedEventData.EventId
     };
   }
 }
