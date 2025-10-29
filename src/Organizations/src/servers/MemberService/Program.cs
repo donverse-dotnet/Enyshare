0892dc179d431914dedf8092a180c34e9018a9ed
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-10-30 02:05:23 +0900

    不要だと思われるコードのコメントアウト。

--- src/Organizations/src/servers/MemberService/Program.cs
+++ src/Organizations/src/servers/MemberService/Program.cs
@@ -18,13 +18,13 @@ if (builder.Environment.IsDevelopment()) {
   builder.Services.AddGrpcReflection();
 }
 
-builder.Services.AddGrpcReflection();
+//builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<OrganizationsMemberServiceImpl>();
-app.MapGrpcReflectionService();
+//app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 if (app.Environment.IsDevelopment()) {
