3cd1774599c3412c4eef26b332bed5098b73c796
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-11-16 09:54:16 +0900

    ユーザーとしてログインするためのLoginAsyncメソッドのテストを追加し、テスト成功。

---
+++ libs/APIClient/test/Pocco.APIClient.Core.Test/LoginTest.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace Pocco.APIClient.Core.Test;
+
+public class LoginTest {
+
+    private readonly ITestOutputHelper _output;
+    private readonly ILogger<APIClient> _logger;
+
+    private readonly APIClient _client;
+
+    public LoginTest(ITestOutputHelper output) {
+        _output = output;
+        _logger = output.BuildLoggerFor<APIClient>();
+
+        var config = new APIClientConfigurations(APIClientType.User);
+        _client = new APIClient(config, _logger);
+    }
+
+    [Fact]
+    public async Task TestLoginAsync() {
+        var email = Environment.GetEnvironmentVariable("POCCO_TEST_EMAIL") ?? string.Empty;
+        var password = Environment.GetEnvironmentVariable("POCCO_TEST_PASSWORD") ?? string.Empty;
+
+        var loginResult = await _client.LoginAsync(email, password);
+        Assert.True(loginResult, "Login should succeed with valid credentials.");
+    }
+}
