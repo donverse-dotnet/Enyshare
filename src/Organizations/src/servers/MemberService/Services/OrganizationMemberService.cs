85fad49f6dd5af58abecd59888e1034ef9a043fc
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-11-18 16:30:24 +0900

    GetList関数の場所が見にくかったため移動。

--- src/Organizations/src/servers/MemberService/Services/OrganizationMemberService.cs
+++ src/Organizations/src/servers/MemberService/Services/OrganizationMemberService.cs
@@ -31,16 +31,6 @@ public class OrganizationsMemberServiceImpl : V0OrganizationMemberService.V0Orga
     _logger.LogInformation("OrganizationsMemberServiceImpl is initialized!");
   }
 
-  public override async Task<V0GetListReply> GetList(V0GetListRequest request, ServerCallContext context) {
-    var members = await _repository.GetListAsync(request.OrganizationId);
-
-    var response = new V0GetListReply();
-    response.Members.AddRange(members.Select(m => new  MemberListResponse {
-      Id = m.Id
-    }));
-
-    return response;
-    }
   /// <summary>
   /// メンバーの新規登録処理
   /// - 組織IDとユーザーIDの重複チェック（論理処理されていないもののみ対象）
@@ -162,5 +152,15 @@ public class OrganizationsMemberServiceImpl : V0OrganizationMemberService.V0Orga
     return new V0GetMemberReply {
       Id = model.Id
     };
+  }
+  public override async Task<V0GetListReply> GetList(V0GetListRequest request, ServerCallContext context) {
+    var members = await _repository.GetListAsync(request.OrganizationId);
+
+    var response = new V0GetListReply();
+    response.Members.AddRange(members.Select(m => new  MemberListResponse {
+      Id = m.Id
+    }));
+
+    return response;
   }
 }
