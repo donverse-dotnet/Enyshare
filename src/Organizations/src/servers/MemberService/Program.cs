4e420aa13d982d4b73f16ce7cfe502875ee12600
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-11-18 16:04:22 +0900

    コードが重複していたので削除

--- src/Organizations/src/servers/MemberService/Program.cs
+++ src/Organizations/src/servers/MemberService/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddSingleton(sp => {
   return new MongoClient(connectionString);
 });
 
-builder.Services.AddSingleton<V0EventReceiver.V0EventReceiverClient>(sp => {
+builder.Services.AddSingleton(sp => {
   var eventBridgeUrl = Environment.GetEnvironmentVariable("EVENTBRIDGE_URL") ?? throw new ArgumentException("EVENTBRIDGE_URL is not found");
   var channel = GrpcChannel.ForAddress(eventBridgeUrl);
   return new V0EventReceiver.V0EventReceiverClient(channel);
@@ -27,29 +27,17 @@ builder.Services.AddSingleton<IMemberRepository>(sp => {
   return new MemberRepository(MongoClient);
 });
 
-builder.Services.AddSingleton<V0EventReceiver.V0EventReceiverClient>(sp => {
-  var eventBridgeUrl = Environment.GetEnvironmentVariable("EVENTBRIDGE_URL") ?? throw new ArgumentException("EVENTBRIDGE_URL is not found");
-  var channel = GrpcChannel.ForAddress(eventBridgeUrl);
-  return new V0EventReceiver.V0EventReceiverClient(channel);
-});
-
 if (builder.Environment.IsDevelopment()) {
   builder.Services.AddGrpcReflection();
 }
 
 builder.Services.AddGrpc();
 
-if (builder.Environment.IsDevelopment()) {
-  builder.Services.AddGrpcReflection();
-}
-
-//builder.Services.AddGrpcReflection();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<OrganizationsMemberServiceImpl>();
-//app.MapGrpcReflectionService();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 if (app.Environment.IsDevelopment()) {
