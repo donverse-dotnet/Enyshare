d7182c7277de492765ff73d74545507a2437242d
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-11-18 14:43:02 +0900

    パッケージの更新に合わせてメソッドパラメータを変更。

--- libs/APIClient/src/Pocco.APIClient.Core/Organizations/Create.cs
+++ libs/APIClient/src/Pocco.APIClient.Core/Organizations/Create.cs
@@ -11,7 +11,7 @@ public partial class APIClient {
     /// <returns>イベントIDを返却します。</returns>
     /// <exception cref="InvalidOperationException">ログインできておらず、セッションデータがないときに投げられます。</exception>
     public async Task<V0EventInvokedResponse> CreateOrganizationAsync(
-        V0CreateXRequest request,
+        V0CreateOrganizationRequest request,
         CancellationToken cancellationToken = default
     ) {
         var sessionData = SessionManager.GetSessionData() ?? throw new InvalidOperationException("Cannot create organization: No session data available.");
