c172ca757d5fa2e9f2664e4563caf0bcf9c42dfe
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-11-21 05:22:09 +0900

    テスト側のログを削除。

--- libs/APIClient/test/AllFeaturedTest/Program.cs
+++ libs/APIClient/test/AllFeaturedTest/Program.cs
@@ -10,7 +10,6 @@ var client = new APIClient(config, logger);
 var email = Environment.GetEnvironmentVariable("POCCO_TEST_EMAIL") ?? throw new Exception("POCCO_TEST_EMAIL is not set.");
 var password = Environment.GetEnvironmentVariable("POCCO_TEST_PASSWORD") ?? throw new Exception("POCCO_TEST_PASSWORD is not set.");
 await client.SessionManager.LoginAsync(email, password);
-Console.WriteLine("Logged in successfully.");
 
 var sessionData = client.SessionManager.GetSessionData() ?? throw new Exception("Session data is null after login.");
 var listenRequest = new ListenRequest {
@@ -24,29 +23,26 @@ await client.EventListener.StartListeningAsync(listenRequest);
 await Task.Delay(2000); // 少し待ってからイベント購読を更新
 
 var randomOrgName = $"TestOrg-{Guid.NewGuid().ToString().Substring(0, 8)}";
-var orgCreateReply = await client.CreateOrganizationAsync(new V0CreateOrganizationRequest {
+await client.CreateOrganizationAsync(new V0CreateOrganizationRequest {
     Base = new V0CreateXRequest {
         Name = randomOrgName,
     },
     Description = "This is a test organization.",
 });
-Console.WriteLine($"Organization creation requested. Event ID: {orgCreateReply.EventId}");
 
 await Task.Delay(5000); // イベント受信のために少し待機
 
 var orgId = client.EventListener.TempOrganizationIds.LastOrDefault() ?? throw new Exception("Organization ID not found from events.");
-var orgUpdareReply = await client.UpdateOrganizationNameAsync(new V0UpdateOrganizationNameRequest {
+await client.UpdateOrganizationNameAsync(new V0UpdateOrganizationNameRequest {
     OrganizationId = orgId,
     Name = randomOrgName + "-Updated",
 });
-Console.WriteLine($"Organization name update requested. Event ID: {orgUpdareReply.EventId}");
 
 await Task.Delay(5000); // イベント受信のために少し待機
 
-var orgDeleteReply = await client.DeleteOrganizationAsync(new V0BaseRequest {
+await client.DeleteOrganizationAsync(new V0BaseRequest {
     Id = orgId,
 });
-Console.WriteLine($"Organization deletion requested. Event ID: {orgDeleteReply.EventId}");
 
 await Task.Delay(5000); // 最後に少し待ってから終了
 client.Dispose();
@@ -69,7 +65,7 @@ class Logger<T> : ILogger<T> {
 }
 static class LoggerFactory {
     public static ILogger<T> Create<T>() where T : class {
-        return new Logger<T>("AllFeaturedTest");
+        return new Logger<T>(typeof(T).Name.Split('.').Last());
     }
 }
 #endregion
