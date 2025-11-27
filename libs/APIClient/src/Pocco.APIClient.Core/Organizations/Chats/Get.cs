a74d8e2933f2556ec7ca3c177cea75dee2878059
Masaya1022 <hyn.masaya.h@gmail.com>
2025-11-27 16:17:04 +0900

    ChatsのGetの、GetListChatsAsyncではなく、ListChatsAsyncのため変更。

--- libs/APIClient/src/Pocco.APIClient.Core/Organizations/Chats/Get.cs
+++ libs/APIClient/src/Pocco.APIClient.Core/Organizations/Chats/Get.cs
@@ -21,14 +21,14 @@ public partial class APIClient {
         return reply;
     }
 
-     public async Task<V0ListChatsResponse> GetListOrganizationChatAsync(
+     public async Task<V0ListChatsResponse> ListOrganizationChatsAsync(
         V0ListXRequest request,
         CancellationToken cancellationToken = default
     ) {
         var sessionData = SessionManager.GetSessionData() ?? throw new InvalidOperationException("Cannot getlist chat: No session data available.");
         var header = sessionData.ToMetadata();
 
-        var reply = await API.GetListChatAsync(request, header, null, cancellationToken); //TODO: わかりやすい名前に変更する
+        var reply = await API.ListChatsAsync(request, header, null, cancellationToken); //TODO: わかりやすい名前に変更する
         return reply;
     }
 }
