e2b02d5d7a9c84fefaec77c78d12668f6ed8f08b
Masaya1022 <hyn.masaya.h@gmail.com>
2025-11-27 15:41:36 +0900

    MessageのDeleteAPIWrapperを作成。

---
+++ libs/APIClient/src/Pocco.APIClient.Core/Organizations/Messages/Delete.cs
@@ -0,0 +1,23 @@
+using Pocco.Libs.Protobufs.CoreAPI.Services;
+
+namespace Pocco.APIClient.Core;
+
+public partial class APIClient {
+    /// <summary>
+    /// メッセージを削除するためのメソッドを提供します。
+    /// </summary>
+    /// <param name="request">メッセージの削除に必要なデータ</param>
+    /// <param name="cancellationToken">このメソッドをキャンセルするためのトークン</param>
+    /// <returns>イベントIDを返却します。</returns>
+    /// <exception cref="InvalidOperationException">ログインできておらず、セッションデータがないときに投げられます。</exception>
+    public async Task<V0EventInvokedResponse> DeleteOrganizationMessageAsync(
+        V0BaseRequest request,
+        CancellationToken cancellationToken = default
+    ) {
+        var sessionData = SessionManager.GetSessionData() ?? throw new InvalidOperationException("Cannot delete message: No session data available.");
+        var header = sessionData.ToMetadata();
+
+        var reply = await API.DeleteMessageAsync(request, header, null, cancellationToken); //TODO: わかりやすい名前に変更する
+        return reply;
+    }
+}
