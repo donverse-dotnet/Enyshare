05a33f2d6ce03dddc8706faaeee0bee9afd51019
Masaya1022 <hyn.masaya.h@gmail.com>
2025-11-27 17:18:56 +0900

    ChatsのGetListのコメントがなかったため、追加。

--- libs/APIClient/src/Pocco.APIClient.Core/Organizations/Chats/Get.cs
+++ libs/APIClient/src/Pocco.APIClient.Core/Organizations/Chats/Get.cs
@@ -21,6 +21,13 @@ public partial class APIClient {
         return reply;
     }
 
+    /// <summary>
+    /// チャットの一覧を取得するためのメソッドを提供します。
+    /// </summary>
+    /// <param name="request">チャットの取得に必要なデータ</param>
+    /// <param name="cancellationToken">このメソッドをキャンセルするためのトークン</param>
+    /// <returns>複数のチャットを返却します。</returns>
+    /// <exception cref="InvalidOperationException">ログインできておらず、セッションデータがないときに投げられます。</exception>
      public async Task<V0ListChatsResponse> ListOrganizationChatsAsync(
         V0ListXRequest request,
         CancellationToken cancellationToken = default
