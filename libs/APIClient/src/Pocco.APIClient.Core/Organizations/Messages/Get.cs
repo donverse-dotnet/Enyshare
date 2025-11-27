c984b7db65ec057cea6c4f6cc7a649509e28bad3
Masaya1022 <hyn.masaya.h@gmail.com>
2025-11-27 15:46:38 +0900

    MessagesのGetとGetListAPIWrapperを作成。

---
+++ libs/APIClient/src/Pocco.APIClient.Core/Organizations/Messages/Get.cs
@@ -0,0 +1,34 @@
+using Pocco.Libs.Protobufs.CoreAPI.Services;
+
+namespace Pocco.APIClient.Core;
+
+public partial class APIClient {
+    /// <summary>
+    /// メッセージを取得するためのメソッドを提供します。
+    /// </summary>
+    /// <param name="request">メッセージの取得に必要なデータ</param>
+    /// <param name="cancellationToken">このメソッドをキャンセルするためのトークン</param>
+    /// <returns>１つのメッセージを返却します。</returns>
+    /// <exception cref="InvalidOperationException">ログインできておらず、セッションデータがないときに投げられます。</exception>
+    public async Task<Message> GetOrganizationMessageAsync(
+        V0BaseRequest request,
+        CancellationToken cancellationToken = default
+    ) {
+        var sessionData = SessionManager.GetSessionData() ?? throw new InvalidOperationException("Cannot get message: No session data available.");
+        var header = sessionData.ToMetadata();
+
+        var reply = await API.GetMessageAsync(request, header, null, cancellationToken); //TODO: わかりやすい名前に変更する
+        return reply;
+    }
+
+    public async Task<V0ListMessagesResponse> GetListOrganizationMessageAsync(
+        V0ListMessagesRequest request,
+        CancellationToken cancellationToken = default
+    ) {
+        var sessionData = SessionManager.GetSessionData() ?? throw new InvalidOperationException("Cannot getlist message: No session data available.");
+        var header = sessionData.ToMetadata();
+
+        var reply = await API.GetListMessageAsync(request, header, null, cancellationToken); //TODO: わかりやすい名前に変更する
+        return reply;
+    }
+}
