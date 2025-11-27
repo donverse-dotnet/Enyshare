a71413b49222d6a07b8d65dc29e20150092437d8
Masaya1022 <hyn.masaya.h@gmail.com>
2025-11-27 15:00:35 +0900

    ChatsのUpdateAPIWrapperを作成。

---
+++ libs/APIClient/src/Pocco.APIClient.Core/Organizations/Roles/Update.cs
@@ -0,0 +1,23 @@
+using Pocco.Libs.Protobufs.CoreAPI.Services;
+
+namespace Pocco.APIClient.Core;
+
+public partial class APIClient {
+    /// <summary>
+    /// ロールを更新するためのメソッドを提供します。
+    /// </summary>
+    /// <param name="request">ロールの変更に必要なデータ</param>
+    /// <param name="cancellationToken">このメソッドをキャンセルするためのトークン</param>
+    /// <returns>イベントIDを返却します。</returns>
+    /// <exception cref="InvalidOperationException">ログインできておらず、セッションデータがないときに投げられます。</exception>
+    public async Task<V0EventInvokedResponse> UpdateOrganizationRoleAsync(
+        Role request,
+        CancellationToken cancellationToken = default
+    ) {
+        var sessionData = SessionManager.GetSessionData() ?? throw new InvalidOperationException("Cannot create role: No session data available.");
+        var header = sessionData.ToMetadata();
+
+        var reply = await API.UpdateOrganizationRoleAsync(request, header, null, cancellationToken); //TODO: わかりやすい名前に変更する
+        return reply;
+    }
+}
