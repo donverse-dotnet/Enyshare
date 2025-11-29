ada517f5a7e0f78d5d7c761452a04187482acf92
STanoue19 <fxb4zffhb@i.softbank.jp>
2025-11-29 18:52:03 +0900

    データベースからOrgIdを取得、削除し、Accountを更新。

--- src/Organizations/src/servers/MemberService/Services/OrganizationMemberService.cs
+++ src/Organizations/src/servers/MemberService/Services/OrganizationMemberService.cs
@@ -8,6 +8,11 @@ using Pocco.Libs.Protobufs.Organizations_Member.Services;
 using Pocco.Libs.Protobufs.EventBridge.Services;
 using Pocco.Libs.Protobufs.EventBridge.Types;
 using Pocco.Libs.Protobufs.EventBridge.Enums;
+using Pocco.Libs.Protobufs.Accounts.Services;
+using Pocco.Libs.Protobufs.Accounts.Types;
+using Pocco.Libs.Protobufs.Accounts.Enums;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 
 namespace MemberService.Services;
@@ -20,14 +25,18 @@ public class OrganizationsMemberServiceImpl : V0OrganizationMemberService.V0Orga
   private readonly IMemberRepository _repository;
   private readonly ILogger<OrganizationsMemberServiceImpl> _logger;
   private readonly V0EventReceiver.V0EventReceiverClient _eventBridge;
+  private readonly V0InternalAccountService.V0InternalAccountServiceClient _internalAccountSvc;
+  private readonly IMongoCollection<MemberEntity> _orgs;
 
   public OrganizationsMemberServiceImpl(
     [FromServices] IMemberRepository repository,
     [FromServices] ILogger<OrganizationsMemberServiceImpl> logger,
+    [FromServices] V0InternalAccountService.V0InternalAccountServiceClient internalAccountSvc,
     [FromServices] V0EventReceiver.V0EventReceiverClient eventBridge
   ) {
     _repository = repository;
     _logger = logger;
+    _internalAccountSvc = internalAccountSvc;
     _eventBridge = eventBridge;
 
     _logger.LogInformation("OrganizationsMemberServiceImpl is initialized!");
@@ -93,6 +102,21 @@ public class OrganizationsMemberServiceImpl : V0OrganizationMemberService.V0Orga
 
     var createdMember = await _repository.CreateAsync(request.OrganizationId, member);
     _logger.LogInformation("{MemberId} is successfully created on {OrganizationId}", createdMember.Id, request.OrganizationId);
+
+    // var org = new MemberEntity {
+    //     Id = ObjectId.GenerateNewId().ToString()
+    // };
+
+    // await _orgs.InsertOneAsync(org);
+
+    var createdOrg = _orgs.FindAsync(item => item.Nickname == request.Name).Result.ToListAsync().Result.FirstOrDefault() ?? throw new RpcException(new Status(StatusCode.NotFound, "Organization mayde created but can't found."));
+
+    //アカウントを更新
+    var reply = await _internalAccountSvc.UpdateOrgListAsync(new V0UpdateOrgListRequest {
+        AccountId = request.CreatedBy,
+        OrgId = createdOrg.Id,
+        Action = V0OrgListUpdateActions.Add
+    });
 
     //　イベントを伝搬させるのをEventBridgeに依頼
     var newEventData = new V0NewEventRequest {
@@ -164,6 +188,15 @@ public class OrganizationsMemberServiceImpl : V0OrganizationMemberService.V0Orga
     if (!deleted) {
       throw new RpcException(new Status(StatusCode.NotFound, "Member not found or no fields to delete"));
     }
+
+    var deletedOrg = _orgs.FindAsync(item => item.Name == request.Name).Result.ToListAsync().Result.FirstOrDefault() ?? throw new RpcException(new Status(StatusCode.NotFound, "Organization maybe created but can't found."));
+
+    //アカウントを更新
+    var reply = await _internalAccountSvc.UpdateOrgListAsync(new V0UpdateOrgListRequest {
+        AccountId = request.DeletedBy,
+        OrgId = deletedOrg.Id,
+        Action = V0OrgListUpdateActions.Remove
+    });
 
     var newEventData = new V0NewEventRequest {
       Topic = V0EventTopics.EventTopicOrganization,
