69ab36755c5a47839f77dde22d188b5bde747568
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-11-29 15:08:38 +0900

    組織の情報の更新イベントを受け取るためのイベントIDを更新。

--- libs/APIClient/src/Pocco.APIClient.Core/Organizations/_Events.cs
+++ libs/APIClient/src/Pocco.APIClient.Core/Organizations/_Events.cs
@@ -3,12 +3,12 @@ using Pocco.Libs.Protobufs.CoreAPI.Services;
 namespace Pocco.APIClient.Core;
 
 public static partial class ClientEvents {
-    public const string ON_ORGANIZATION_INFO_CREATED = "OnInfoCreated";
+    public const string ON_ORGANIZATION_INFO_CREATED = "OnOrganizationInfoCreated";
     public record OnOrganizationInfoCreated(string EventId, Organization Organization) : BaseEvent(EventId);
 
-    public const string ON_ORGANIZATION_INFO_UPDATED = "OnInfoUpdated"; // TODO: 名前ではなく情報すべての更新に変更する
+    public const string ON_ORGANIZATION_INFO_UPDATED = "OnOrganizationInfoUpdated"; // TODO: 名前ではなく情報すべての更新に変更する
     public record OnOrganizationInfoUpdated(string EventId, Organization NewOrganization) : BaseEvent(EventId);
 
-    public const string ON_ORGANIZATION_INFO_DELETED = "OnInfoDeleted";
+    public const string ON_ORGANIZATION_INFO_DELETED = "OnOrganizationInfoDeleted";
     public record OnOrganizationInfoDeleted(string EventId, string OrganizationId) : BaseEvent(EventId);
 }
