8cd84927cc51b112a01067679366c9971c89e22a
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-12-04 14:34:59 +0900

    デバッグかどうかをWebHostから取得するように変更。

--- src/Accounts/src/service/Program.cs
+++ src/Accounts/src/service/Program.cs
@@ -1,7 +1,6 @@
 using Pocco.Svc.Accounts.Services;
 using MongoDB.Driver;
 
-#if DEBUG
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IMongoClient>(sp => {
@@ -13,39 +12,18 @@ builder.Services.AddSingleton<IMongoClient>(sp => {
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddGrpcReflection();
+if (builder.Environment.IsDevelopment()) {
+  builder.Services.AddGrpcReflection();
+}
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<UserAccountsService>();
 app.MapGrpcService<InternalAccountServiceImpl>();
-app.MapGrpcReflectionService();
+if (app.Environment.IsDevelopment()) {
+  app.MapGrpcReflectionService();
+}
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
-
-#else
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton<IMongoClient>(sp => {
-    var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_URI") ?? throw new ArgumentException("TEST_DATABASE_URI is not found");
-
-    return new MongoClient(connectionString);
-});
-
-
-// Add services to the container.
-builder.Services.AddGrpc();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<UserAccountsService>();
-
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-app.Run();
-#endif
