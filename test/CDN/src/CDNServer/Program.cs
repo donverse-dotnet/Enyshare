9913f79697d9aca960fafb333991a5ae0e140278
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-12-24 08:13:07 +0900

    クロスオリジンリクエストに対応。

--- test/CDN/src/CDNServer/Program.cs
+++ test/CDN/src/CDNServer/Program.cs
@@ -5,10 +5,20 @@ public class Server {
   public static void Main(string[] args) {
     var builder = WebApplication.CreateBuilder(args);
 
+    builder.Services.AddCors(options => {
+      options.AddPolicy("AllowUIRenderer", policy => {
+        policy.WithOrigins("http://localhost:5099")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+      });
+    });
+
     builder.Services.AddOpenApi();
     builder.Services.AddControllers();
 
     var app = builder.Build();
+
+    app.UseCors("AllowUIRenderer");
 
     if (app.Environment.IsDevelopment()) {
       app.MapOpenApi();
