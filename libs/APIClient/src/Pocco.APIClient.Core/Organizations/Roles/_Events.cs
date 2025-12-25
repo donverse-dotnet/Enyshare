8a7bc1c6532e392059d8d94f382104a74e8e3a9c
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2025-12-26 07:46:07 +0900

    イベント名格納用変数の命名規則の統一。

--- libs/APIClient/src/Pocco.APIClient.Core/Organizations/Roles/_Events.cs
+++ libs/APIClient/src/Pocco.APIClient.Core/Organizations/Roles/_Events.cs
@@ -3,12 +3,12 @@ using Pocco.Libs.Protobufs.CoreAPI.Services;
 namespace Pocco.APIClient.Core;
 
 public static partial class ClientEvents {
-    public const string ON_ORGANIZATION_Role_CREATED = "OnOrganizationRoleCreated";
+    public const string ON_ORGANIZATION_ROLE_CREATED = "OnOrganizationRoleCreated";
     public record OnOrganizationRoleCreated(string EventId, Role Role) : BaseEvent(EventId);
 
-    public const string ON_ORGANIZATION_Role_UPDATED = "OnOrganizationRoleUpdated"; // TODO: 名前ではなく情報すべての更新に変更する
+    public const string ON_ORGANIZATION_ROLE_UPDATED = "OnOrganizationRoleUpdated"; // TODO: 名前ではなく情報すべての更新に変更する
     public record OnOrganizationRoleUpdated(string EventId, Role NewRole) : BaseEvent(EventId);
 
-    public const string ON_ORGANIZATION_Role_DELETED = "OnOrganizationRoleDeleted";
+    public const string ON_ORGANIZATION_ROLE_DELETED = "OnOrganizationRoleDeleted";
     public record OnOrganizationRoleDeleted(string EventId, string RoleId) : BaseEvent(EventId);
 }
