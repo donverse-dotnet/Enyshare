040fac1334850434ef47eb5d03c6c7297df30eb6
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2026-01-10 18:33:53 +0900

    DIの構成によってサービスが停止する問題を修正。

--- src/Organizations/src/servers/RoleService/Program.cs
+++ src/Organizations/src/servers/RoleService/Program.cs
@@ -47,7 +47,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<OrganizationRoleService>();
 app.MapGrpcService<V0InternalRoleServiceImpl>();
-app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 if (app.Environment.IsDevelopment()) {
